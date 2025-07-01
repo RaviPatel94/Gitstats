571c27ad87bb3efd0e5d6809cc1b38be004b0178
RaviPatel94 <ravi942004@gmail.com>
2025-07-01 15:32:25 +0530

    fixing import deployment issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import EnhancedHeroPage from "@/components/Card/Hero";
+import EnhancedHeroPage from '../components/Card/Hero'
 import './globals.css';
 export default function Home() {
   return (
     <div>
       <EnhancedHeroPage />
-    
     </div>
   );
 }
