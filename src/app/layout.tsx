db6b2fb3bbb01b9c62196637ee164e336d1f4fe4
RaviPatel94 <ravi942004@gmail.com>
2025-10-10 09:09:08 +0530

    google verification added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Github Card",
+  title: "Github Card generator",
   description: "Create yourself a github card",
   keywords: [
     "GitHub Card",
